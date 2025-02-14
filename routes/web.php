f96ab3255b4a2750cf3ff12e3da14fdb16590d04
Code002 <kakashivncm123@gmail.com>
2025-02-14 14:29:16 +0700

    Update Route

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,32 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/test', function () {
-    return ('welcome');
+// ----------------------- Route Admin -----------------------
+Route::get('/admin', function () {
+    return view('admin.index');
 });
 
+Route::get('admin/widgets/small-box', function () {
+    return view('admin.components.widgets.small-box'); 
+})->name('admin.small-box');
+//---------------------------------------------------------------------
+
+// ----------------------- Route Auth -----------------------
+Route::get('/login', function () {
+    return view('auth.components.login');
+})->name("login");
+
+Route::get('/register', function () {
+    return view('auth.components.register');
+})->name("register");
+
+Route::get('/forgot', function () {
+    return view('auth.components.forgot');
+})->name("forgot");
+//---------------------------------------------------------------------
+
+// ----------------------- Route Home -----------------------
+Route::get('/home', function () {
+    return ('auth.components.home');
+})->name("home");
+//---------------------------------------------------------------------
