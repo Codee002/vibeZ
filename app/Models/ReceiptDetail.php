a16465527791ca7e5430dbe254613e1cbaad5238
Code002 <kakashivncm123@gmail.com>
2025-03-03 23:53:15 +0700

    Update Model ReceiDetail

--- app/Models/ReceiptDetail.php
+++ app/Models/ReceiptDetail.php
@@ -2,9 +2,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ReceiptDetail extends Model
 {
+
+    use SoftDeletes;
     protected $fillable = [
         'receipt_id',
         'product_id',
