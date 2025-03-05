75b8197aca997de539b7e4d55d97632ad6d0bb71
Code002 <kakashivncm123@gmail.com>
2025-03-05 23:04:05 +0700

    Update View Auth

--- resources/views/auth/components/register.blade.php
+++ resources/views/auth/components/register.blade.php
@@ -8,105 +8,93 @@
     <div class="row">
         <div class="">
             <div class="form-wrapper">
-                <form action="register/store" method="POST">
+                <form action="{{ route('register') }}" method="POST">
+                    @csrf
 
                     {{-- LOGO --}}
-                    <a href="{{ route("home") }}">
+                    <a href="{{ route('home') }}">
                         <img src="assets/images/logo/register-format.png" class="w-100 logo" alt="">
                     </a>
-
                     <!-- FLASH MESSAGES -->
-                    <?//php if (!empty($msg)): ?>
-                    <?//php foreach ($msg as $key => $value): ?>
-                    <div class="alert alert-<?//= e($key) ?> text-center">
-                        <strong>
-                            <?//= e($value) ?>
-                        </strong>
-                    </div>
-                    <?//php endforeach; ?>
-                    <?//php endif ?>
+                    @if (session('success'))
+                        <div class="alert alert-success">
+                            {{ session('success') }}
+                        </div>
+                    @endif
 
-                    <!-- Fix CSRF -->
-                    <input type="hidden" name="token" value="<?//= $_SESSION['token'] ?>">
-
+                    @if (session('danger'))
+                        <div class="alert alert-danger">
+                            {{ session('danger') }}
+                        </div>
+                    @endif
+                    
                     <!-- Name -->
                     <div class="form-group  mg-form">
                         <div class="form-floating mb-3 mt-3">
-                            <input type="text" class="form-control <?//= isset($errors['name']) ? 'is-invalid' : '' ?>"
-                                id="name" placeholder="Enter name" name="name" autocomplete="off"
-                                value="<?//= isset($oldForm) ? e($oldForm['name']) : '' ?>">
+                            <input type="text"
+                                class="form-control
+                              @error('name') is-invalid @enderror"
+                                value="{{ old('name') }}" id="name" placeholder="Enter name" name="name"
+                                autocomplete="off">
                             <label class="label-input" for="name">Họ tên</label>
                         </div>
-                        <?//php if (isset($errors['name'])): ?>
-                        <?//php foreach ($errors['name'] as $value): ?>
-                        <span class="invalid-feedback" style="display: block">
-                            <strong>
-                                <?//= e($value) ?>
-                            </strong>
-                        </span>
-                        <?//php endforeach ?>
-                        <?//php endif ?>
+                        @error('name')
+                            <span class="invalid-feedback" style="display: block">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
                     </div>
 
                     <!-- Username -->
                     <div class="form-group  mg-form">
                         <div class="form-floating mb-3 mt-3">
                             <input type="text"
-                                class="form-control <?//= isset($errors['username']) ? 'is-invalid' : '' ?>" id="username"
-                                placeholder="Enter username" name="username" autocomplete="off"
-                                value="<?//= isset($oldForm) ? e($oldForm['username']) : '' ?>">
+                                class="form-control 
+                                @error('username') is-invalid @enderror"
+                                value="{{ old('username') }}" id="username" placeholder="Enter username" name="username"
+                                autocomplete="off">
                             <label class="label-input" for="username">Tên đăng nhập</label>
                         </div>
-                        <?//php if (isset($errors['username'])): ?>
-                        <?//php foreach ($errors['username'] as $value): ?>
-                        <span class="invalid-feedback" style="display: block">
-                            <strong>
-                                <?//= e($value) ?>
-                            </strong>
-                        </span>
-                        <?//php endforeach ?>
-                        <?//php endif ?>
+                        @error('username')
+                            <span class="invalid-feedback" style="display: block">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
                     </div>
 
                     <!-- Password -->
                     <div class="form-group  mg-form">
                         <div class="form-floating mt-3 mb-3">
                             <input type="password"
-                                class="form-control <?//= isset($errors['password']) ? 'is-invalid' : '' ?> pwd-mg"
+                                class="form-control pwd-mg
+                                @error('password') is-invalid @enderror"
                                 id="password" placeholder="Enter password" name="password" style="background-image: none">
                             <i class="fa-solid fa-eye pwd-eye"></i>
                             <label class="label-input" for="password">Mật khẩu</label>
                         </div>
-                        <?//php if (isset($errors['password'])): ?>
-                        <?//php foreach ($errors['password'] as $value): ?>
-                        <span class="invalid-feedback" style="display: block">
-                            <strong>
-                                <?//= e($value) ?>
-                            </strong>
-                        </span>
-                        <?//php endforeach ?>
-                        <?//php endif ?>
+                        @error('password')
+                            <span class="invalid-feedback" style="display: block">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
                     </div>
 
                     <!-- PassConfirm -->
                     <div class="form-group  mg-form">
                         <div class="form-floating mt-3 mb-3">
                             <input type="password"
-                                class="form-control <?//= isset($errors['password_confirm']) ? 'is-invalid' : '' ?> password-mg"
-                                id="password_confirm" placeholder="Enter password_confirm" name="password_confirm"
+                                class="form-control password-mg
+                                @error('password_confirmation') is-invalid @enderror"
+                                id="password_confirmation" placeholder="Enter password_confirm" name="password_confirmation"
                                 style="background-image: none">
                             <i class="fa-solid fa-eye pwd-eye"></i>
-                            <label class="label-input" for="password_confirm">Nhập lại mật khẩu</label>
+                            <label class="label-input" for="password_confirmation">Nhập lại mật khẩu</label>
                         </div>
-                        <?//php if (isset($errors['password_confirm'])): ?>
-                        <?//php foreach ($errors['password_confirm'] as $value): ?>
-                        <span class="invalid-feedback" style="display: block">
-                            <strong>
-                                <?//= e($value) ?>
-                            </strong>
-                        </span>
-                        <?//php endforeach ?>
-                        <?//php endif ?>
+                        @error('password_confirmation')
+                            <span class="invalid-feedback" style="display: block">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
                     </div>
 
                     <!-- Capcha -->
@@ -114,32 +102,35 @@
                         <div class="form-floating mt-3 mb-0 d-flex justify-content-start" style="align-items: center"
                             id="capcha-group">
                             <input type="text"
-                                class="form-control  <?//= isset($errors['capcha']) ? 'is-invalid' : '' ?>" placeholder=""
-                                name="capcha">
+                                class="form-control 
+                                 @error('captcha') is-invalid @enderror"
+                                placeholder="" name="captcha">
                             <label class="label-input" for="">Capcha</label>
 
-                            <img id="capcha-img" src="<?//php echo $capcha->inline(); ?>" style="height: 100%"
-                                class="ms-3 me-3" />
+                            <img id="capcha-img" src="{{ $captcha->inline() }}" style="height: 100%" class="ms-3 me-3" />
 
                             <i id="reload-capcha" onclick="reloadCapcha()" class="fa-solid fa-rotate-right"></i>
                         </div>
-                        <?//php if (isset($errors['capcha'])): ?>
-                        <?//php foreach ($errors['capcha'] as $value): ?>
-                        <span class="invalid-feedback" style="display: block">
-                            <strong>
-                                <?//= e($value) ?>
-                            </strong>
-                        </span>
-                        <?//php endforeach ?>
-                        <?//php endif ?>
+                        @error('captcha')
+                            <span class="invalid-feedback" style="display: block">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
                     </div>
 
                     <div class="form-group  mg-form">
-                        <label for="accept"><input required type="checkbox" value="1" id="accept" name="accept"
-                                class="form-check-input"> Bằng
+                        <label for="accept"><input type="checkbox" value="1" id="accept" name="accept"
+                                class="form-check-input 
+                                 @error('accept') is-invalid @enderror">
+                            Bằng
                             việc Đăng ký, bạn đã đọc và đồng ý với
                             <a href="" class="text-decoration-none">Điều khoản sử dụng</a> và
                             <a href="" class="text-decoration-none">Chính sách bảo mật</a> của chúng tôi</label>
+                        @error('accept')
+                            <span class="invalid-feedback" style="display: block">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
                     </div>
 
                     <button type="submit" class="btn btn-primary mg-btn">Đăng ký</button>
