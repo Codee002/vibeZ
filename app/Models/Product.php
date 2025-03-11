cfb18b524be16b1addfab36566d232818dbb4145
Code002 <kakashivncm123@gmail.com>
2025-03-11 23:09:47 +0700

    Update Product Model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,6 +3,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\DB;
 
 class Product extends Model
 {
@@ -40,25 +41,9 @@ class Product extends Model
         return $this->hasMany(SalePrice::class);
     }
 
-    public function getSalePrice($size)
-    {
-        foreach ($this->sale_prices as $detail) {
-            if ($detail['size'] == $size) {
-                return $detail['price'];
-            }
-        }
-        return NULL;
-    }
-
-
     public function warehouse_details()
     {
         return $this->hasMany(WarehouseDetail::class);
-    }
-
-    public function getTotalWarehouse()
-    {
-        return ($this->warehouse_details->sum("quantity"));
     }
 
     public function receipt_details()
@@ -66,6 +51,69 @@ class Product extends Model
         return $this->hasMany(ReceiptDetail::class);
     }
 
+    // ----------------------------------------------------------------------------------------------
+    /**
+     * Lấy ra tất cả các sản phẩm
+     *          Đang kích hoạt
+     *          Không tính theo Size
+     *          Tính chung tất cả các kho
+     * Trả về Id, Tên Sản Phẩm, Tên danh mục
+     */
+    public static function getAllProduct($perPage = null, $search = null, $category = null)
+    {
+        $query = WarehouseDetail::select(
+            "warehouse_details.product_id",
+            // "warehouse_details.size",
+            "warehouse_details.status",
+            DB::raw('SUM(warehouse_details.quantity) as totalQuantity'),
+            "products.name as product_name",    // Lấy tên sản phẩm
+            "categories.id as category_id",     // Lấy mã danh mục
+            "categories.name as category_name", // Lấy tên danh mục
+        )
+            ->join("products", "warehouse_details.product_id", "=", "products.id")
+            ->join("categories", "products.category_id", "=", "categories.id")
+            ->groupBy("warehouse_details.product_id",
+                "warehouse_details.status", "products.name", "categories.name", "categories.id") // Nhóm theo id, size và status
+            ->having("totalQuantity", ">", 0);
+
+        // Tìm theo tên
+        if ($search) {
+            $query->where("products.name", "LIKE", "%" . $search . "%");
+        }
+        // Tìm theo danh mục
+        if ($category) {
+            $query->where("categories.id", $category);
+        }
+
+        // Phân trang
+        if (! empty($perPage)) {
+            $query = $query->paginate($perPage);
+        } else {
+            $query = $query->get();
+        }
+
+        // dd($query->toSql());
+        return $query;
+    }
+
+    // Lấy ra giá sản phẩm của từng size nếu có
+    public function getSalePrice($size)
+    {
+        foreach ($this->sale_prices as $detail) {
+            if ($detail['size'] == $size) {
+                return $detail['price'];
+            }
+        }
+        return null;
+    }
+
+    // Lấy ra tổng sản phẩm trong tất cả các kho
+    public function getTotalWarehouse()
+    {
+        return ($this->warehouse_details->sum("quantity"));
+    }
+
+    // Lấy giá nhập của sản phẩm nếu có
     public function getPurchasePrice($size)
     {
         // dd($this->receipt_details);
