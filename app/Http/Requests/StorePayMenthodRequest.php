3565a13d7819100f9cfad0bc3f7eb7186b7161ca
Code002 <kakashivncm123@gmail.com>
2025-04-07 21:01:40 +0700

    Add Payment Request

---
+++ app/Http/Requests/StorePayMenthodRequest.php
@@ -0,0 +1,45 @@
+<?php
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StorePayMenthodRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            "name" => 'required|unique:payment_methods|max:255',
+            "status" => 'required',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            "required" => ':attribute không được rỗng',
+            "unique"   => ':attribute đã tồn tại',
+            "max"   => ':attribute không được lớn hơn 255 ký tự',
+        ];
+    }
+
+    public function attributes()
+    {
+        return [
+            "name" => 'Tên phương thức',
+            "status" => 'Trạng thái',
+        ];
+    }
+}
