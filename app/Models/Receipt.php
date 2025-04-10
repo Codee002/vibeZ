0ab09ffc8a70fe9e7ecfd01586c28f8496ea9dd4
Code002 <kakashivncm123@gmail.com>
2025-04-11 00:34:06 +0700

    Update Receipt Model

--- app/Models/Receipt.php
+++ app/Models/Receipt.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
@@ -23,9 +22,98 @@ class Receipt extends Model
         return $this->hasMany(ReceiptDetail::class);
     }
 
-    
+    // ---------------- Function -------------
     public function getQuantity()
     {
         return $this->hasMany(ReceiptDetail::class)->sum('quantity');
+    }
+
+    // Lấy tổng giá nhập SP (status = completed)
+    public static function getPurchasePrice($productId)
+    {
+        $purchasePrice = 0;
+        $receipts      = Receipt::with("receipt_details")->get()->all();
+        foreach ($receipts as $receipt) {
+            if ($receipt['status'] == "completed") {
+                foreach ($receipt->receipt_details as $detail) {
+                    if ($detail['product_id'] == $productId) {
+                        $purchasePrice += $detail['purchase_price'] * $detail['quantity'];
+                    }
+
+                }
+            }
+        }
+        return $purchasePrice;
+    }
+
+    // Lấy tổng SL nhập SP (status = completed)
+    public static function getQuantityProduct($productId)
+    {
+        $quantity = 0;
+        $receipts = Receipt::with("receipt_details")->get()->all();
+        foreach ($receipts as $receipt) {
+            if ($receipt['status'] == "completed") {
+                foreach ($receipt->receipt_details as $detail) {
+                    if ($detail['product_id'] == $productId) {
+                        $quantity += $detail['quantity'];
+                    }
+
+                }
+            }
+        }
+        return $quantity;
+    }
+
+    // Lấy tổng tiền nhập hàng (status = completed)
+    public static function getTotalPriceCompleted($startDate = "", $endDate = "")
+    {
+        $price    = 0;
+        $receipts = Receipt::with("receipt_details")
+            ->where("status", "completed");
+
+        // Kiểm tra nếu $startDate có giá trị
+        if ($startDate !== "") {
+            $receipts->where("created_at", ">=", $startDate);
+            // dd($receipts);
+        }
+
+        // Kiểm tra nếu $endDate có giá trị
+        if ($endDate !== "") {
+            $receipts->where("created_at", "<=", $endDate);
+        }
+
+        $receipts = $receipts->get()
+            ->all();
+
+        foreach ($receipts as $receipt) {
+            foreach ($receipt->receipt_details as $detail) {
+                $price += $detail['purchase_price'] * $detail['quantity'];
+            }
+        }
+        return $price;
+    }
+
+    // Lấy tổng số lượng nhập (status = completed)
+    public static function getTotalQuantityCompleted($startDate = "", $endDate = "")
+    {
+        $quantity = 0;
+        $receipts = Receipt::with("receipt_details")
+            ->where("status", "completed");
+
+        // Kiểm tra nếu $startDate có giá trị
+        if ($startDate !== "") {
+            $receipts->where("created_at", ">=", $startDate);
+        }
+
+        // Kiểm tra nếu $endDate có giá trị
+        if ($endDate !== "") {
+            $receipts->where("created_at", "<=", $endDate);
+        }
+        $receipts = $receipts->get()
+            ->all();
+        foreach ($receipts as $receipt) {
+            $quantity += $receipt->receipt_details->sum("quantity");
+        }
+        return $quantity;
     }
 }
